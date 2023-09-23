1842048a50bcd5ffa5ecda830f257cb001bf76f7
19023 <gups_kalsi@outlook.com>
2023-09-22 21:11:44 -0300

    Add project files.

---
+++ passByRefOutputParameter/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace passByRefOutputParameter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.countriesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 25;
+            this.countriesListBox.Location = new System.Drawing.Point(108, 50);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.countriesListBox.Size = new System.Drawing.Size(516, 254);
+            this.countriesListBox.TabIndex = 0;
+            // 
+            // countriesButton
+            // 
+            this.countriesButton.Location = new System.Drawing.Point(108, 353);
+            this.countriesButton.Name = "countriesButton";
+            this.countriesButton.Size = new System.Drawing.Size(178, 60);
+            this.countriesButton.TabIndex = 1;
+            this.countriesButton.Text = "Get Countries";
+            this.countriesButton.UseVisualStyleBackColor = true;
+            this.countriesButton.Click += new System.EventHandler(this.countriesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(430, 353);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(194, 60);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "&Exit && &Save";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.countriesButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.countriesButton);
+            this.Controls.Add(this.countriesListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox countriesListBox;
+        private System.Windows.Forms.Button countriesButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ passByRefOutputParameter/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace passByRefOutputParameter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetFileName(out string selectedFile) // method with a "out" parameter variable.
+                                                          // means you don't have to initialize a variable
+                                                          // before passing it as a reference.
+        {
+            if (openFile.ShowDialog() == DialogResult.OK) // whether a user selected OK in dialog box.
+            {
+                selectedFile = openFile.FileName; // FileName object should be blank in order
+                                                  // to store file name in it.
+                                                  // it will then stored in selectedFile variable.
+            }
+            else
+            {
+                selectedFile = " ";
+            }
+        }
+        private void GetCountries(string files)
+        {
+            try
+            {
+                string countryName;
+
+                // StreamReader inputFile; 
+
+                                        // StreamReader is a class and inputFile is a variable
+                                        // that is declared as StreamReader class to reference a object.
+                                        // It will reference a 
+                                        // file object.
+
+                StreamReader inputFile = new StreamReader(files);
+
+                inputFile = System.IO.File.OpenText(files);
+
+                countriesListBox.Items.Clear();
+
+                while (!inputFile.EndOfStream) // task performed on inputFile variable.
+                {
+                    countryName = inputFile.ReadLine();
+                    countriesListBox.Items.Add(countryName);
+                }
+                inputFile.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+              
+        }
+        private void countriesButton_Click(object sender, EventArgs e)
+        {
+            string fileName; // you do not have to initialize an argument 
+                             // to be passed as reference using "output" parameter variable.
+
+            GetFileName(out fileName); // file name selected is saved in string variable.
+            GetCountries(fileName);
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /*  openFile.initialDirectory = "C:\Data";
+ Verify the user have selected a Open button or Cancel button.
+ if(openFileDialog.ShowDialog()== DialogResult.Cancel)
+{
+    MessageBox.Show("You clicked Cancel Button.");
+}
+ else
+{
+    MessageBox.Show("You have to open or close a file.");
+} */
+    }
+}
